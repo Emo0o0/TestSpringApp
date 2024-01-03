b8ca5227efeeee82730283dd6539587467fe8e6c
Emo0o0 <emil.kolev1102@gmail.com>
2024-01-03 22:11:40 +0200

    MRPHome page

--- src/main/java/com/example/testspringapp/controllers/MRPHomeController.java
+++ src/main/java/com/example/testspringapp/controllers/MRPHomeController.java
@@ -1,9 +1,11 @@
 package com.example.testspringapp.controllers;
 
+import com.example.testspringapp.configs.FxmlView;
 import com.example.testspringapp.configs.StageManager;
 import com.example.testspringapp.persistence.entities.Customer;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,19 @@ import org.springframework.stereotype.Component;
 public class MRPHomeController {
 
     @FXML
-    private ListView<Customer> listView;
+    private Label registerProductLabel;
+    @FXML
+    private Label scrappingCriteriaLabel;
+    @FXML
+    private Label registerCustomerLabel;
+    @FXML
+    private Label addProductsLabel;
+    @FXML
+    private Label removeProductsLabel;
+    @FXML
+    private Label scrapProductLabel;
+    @FXML
+    private Label leaveLabel;
 
     private final StageManager stageManager;
 
@@ -21,4 +35,40 @@ public class MRPHomeController {
     public MRPHomeController(StageManager stageManager) {
         this.stageManager = stageManager;
     }
+
+    public void registerProductHover(){
+        registerProductLabel.setUnderline(true);
+    }
+    public void registerProductHoverExit(){
+        registerProductLabel.setUnderline(false);
+    }
+
+    public void registerProduct() {
+
+    }
+
+    public void setScrappingCriteria() {
+
+    }
+
+    public void registerClient() {
+
+    }
+
+    public void addProductToClientCard() {
+
+    }
+
+    public void removeProductFromClientCard() {
+
+    }
+
+    public void scrapProduct() {
+
+    }
+
+    public void leave() {
+        stageManager.switchScene(FxmlView.LOGIN);
+    }
+
 }
